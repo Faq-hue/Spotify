b15e1d70979bf7a4999e1682bbeb9916743ec91f
Angelo Manfredi <69202121+angelo59930@users.noreply.github.com>
2021-06-04 16:35:25 -0300

    Update Song.java

--- com.Spotify/model/Song.java
+++ com.Spotify/model/Song.java
@@ -2,9 +2,7 @@ package model;
 
 public class Song extends Track {
   public Song(float duration, String name, String gender) {
-    setDuration(duration);
-    setName(name);
-    setGender(gender);
+    super(name, gender, duration, 0);
   }
 
   @Override
