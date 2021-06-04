f06d8cd6e66d5845f534a698bafb7dacf47fe1cf
Angelo Manfredi <69202121+angelo59930@users.noreply.github.com>
2021-06-04 16:35:53 -0300

    Update Podcast.java

--- com.Spotify/model/Podcast.java
+++ com.Spotify/model/Podcast.java
@@ -1,5 +1,19 @@
 package model;
 
-public class Podcast {
-    
+public class Podcast extends Track {
+  private String description;
+
+  public Podcast(String name,String gender, float duration, String description) {
+    super(name, gender, duration, 0);
+    setDescription(description);
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
 }
