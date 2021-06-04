e11d1d5ac81881c50f2aceb28aa18075e6faf987
Facu <69550808+Faq-hue@users.noreply.github.com>
2021-06-04 18:55:01 -0300

    Add files via upload

---
+++ com.Spotify/dao/ConsumerDAO.java
@@ -0,0 +1,45 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import config.JDBCUtil;
+import model.Consumer;
+
+public class ConsumerDAO {
+    
+    public Consumer add ( Consumer consumer ){
+
+        String INSERT_CONSUMER_SQL = "INSERT INTO spotify.consumidor (seguidores, seguidos, Usuario_idUsuario)" + "VALUES (?,?,?);";
+
+        
+
+        try {
+            
+            Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(), JDBCUtil.getPassword());
+
+            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_CONSUMER_SQL);            
+            preparedStatement.setInt(1, consumer.getFollowers());
+            preparedStatement.setInt(2, consumer.getFollowed());
+            preparedStatement.setInt(3, 121);
+
+            System.out.println(preparedStatement);
+
+            preparedStatement.executeUpdate();
+
+            //ResultSet rs = preparedStatement.getGeneratedKeys();
+
+            //if(rs.next()){
+              //  consumer.setId(rs.getInt(1));
+            //}
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return consumer;
+    }
+
+}
