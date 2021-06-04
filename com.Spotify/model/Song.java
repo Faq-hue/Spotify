79cb7f827766784785c0de60af97d65749b9adee
Angelo Manfredi <69202121+angelo59930@users.noreply.github.com>
2021-06-04 16:28:54 -0300

    Update Song.java

--- com.Spotify/model/Song.java
+++ com.Spotify/model/Song.java
@@ -1,5 +1,22 @@
 package model;
 
-public class Song {
-    
+public class Song extends Track {
+  public Song(float duration, String name, String gender) {
+    setDuration(duration);
+    setName(name);
+    setGender(gender);
+  }
+
+  @Override
+  public String toString() {
+    return "name: " + getName() +
+          "\n gender: " + getGender() + 
+          "\n duration of song: " + getDuration();
+  }
+
+  @Override
+  protected Object clone() throws CloneNotSupportedException {
+    return new Song(getDuration(), getName(), getGender());
+  }
 }
+
