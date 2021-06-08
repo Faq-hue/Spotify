3c4bb9bdf7edd5c8801a68ee7ba419a8eb9f8764
Angelo Manfredi <69202121+angelo59930@users.noreply.github.com>
2021-06-08 20:16:28 -0300

    Add files via upload

--- com.Spotify/dao/dao_interfaces/IDao.java
+++ com.Spotify/dao/dao_interfaces/IDao.java
@@ -3,9 +3,9 @@ package dao.dao_interfaces;
 public interface IDao<T> {
   public T add(T arg);
 
-  public T get(int arg);
+  public T get(String arg);
 
-  public T delete(int arg);
+  public T delete(String arg);
 
-  public T update(int arg1,T arg2);
+  public T update(String arg1,T arg2);
 }
