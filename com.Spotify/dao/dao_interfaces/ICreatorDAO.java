9279193d8ef48c4e6757660d493948ab6c7ce858
Angelo Manfredi <69202121+angelo59930@users.noreply.github.com>
2021-06-09 17:43:27 -0300

    Add files via upload

---
+++ com.Spotify/dao/dao_interfaces/ICreatorDAO.java
@@ -0,0 +1,10 @@
+package dao.dao_interfaces;
+
+import java.util.List;
+
+import model.Creator;
+
+public interface ICreatorDAO extends IDao<Creator>{
+  public List<Creator> getlist();
+  public Creator modTracksCreated(String id,byte arg);
+}
