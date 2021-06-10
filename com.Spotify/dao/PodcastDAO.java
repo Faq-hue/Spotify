fa38ff35317770dd023d75ae807f4a2c207b2636
Facu <69550808+Faq-hue@users.noreply.github.com>
2021-06-10 09:17:08 -0300

    Add files via upload

--- com.Spotify/dao/PodcastDAO.java
+++ com.Spotify/dao/PodcastDAO.java
@@ -5,38 +5,138 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import config.JDBCUtil;
+import dao.dao_interfaces.IPodcastDAO;
 import model.Podcast;
 
-public class PodcastDAO {
-    public void add(Podcast podcast) {
+public class PodcastDAO implements IPodcastDAO {
 
-       String INSERT_USER_SQL = "INSERT INTO Spotify.Podcast (descripcion, Pista_idPista)"
-            + "VALUES(?,?)";
-    
-        try (
-            Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
-                JDBCUtil.getPassword());
-            PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER_SQL,
-                Statement.RETURN_GENERATED_KEYS);) {
-    
+  private String INSERT_PODCAST_SQL = "INSERT INTO podcast (id_pista,descripcion)" + "VALUES(?,?)";
 
-          
-          preparedStatement.setString(1, podcast.getDescription());
-         
-          System.out.println(preparedStatement);
-    /* 
-          ResultSet rs = preparedStatement.getGeneratedKeys();
-    
-          if (rs.next()) {
-    
-            podcast.(rs.getInt(2));
-          } */
-    
-        } catch (SQLException e) {
-          System.out.println(e);
-        }
+  private String SELECT_ONE_PODCAST_SQL = "SELECT * FROM podcast " + "WHERE id_pista=?;";
+
+  private String DELETE_ONE_PODCAST_SQL = "DELETE FROM podcast " + "WHERE id_pista=?;";
+
+  private String UPDATE_ONE_PODCAST_SQL = "UPDATE podcast " + "SET descripcion=? WHERE id_pista = ?;";
+
+  private String SELECT_PODCAST_SQL = "SELECT * FROM podcast";
+
+  private TrackDAO tr = new TrackDAO();
+
+  @Override
+  public Podcast add(Podcast podcast) {
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(INSERT_PODCAST_SQL,
+            java.sql.Statement.RETURN_GENERATED_KEYS);) {
+
+      preparedStatement.setString(1, tr.add(podcast).getId());
+      preparedStatement.setString(2, podcast.getDescription());
+
+      System.out.println(preparedStatement);
+
+      preparedStatement.executeUpdate();
+
+    } catch (SQLException e) {
+      System.out.println(e);
+    }
+
+    return podcast;
+  }
+
+  @Override
+  public Podcast get(String id) {
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ONE_PODCAST_SQL);) {
+
+      preparedStatement.setString(1, id);
+
+      ResultSet rs = preparedStatement.executeQuery();
+      while (rs.next()) {
+        return new Podcast(rs.getString(1), rs.getString(2), rs.getFloat(3));
       }
+
+      rs.close();
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
+    return null;
+  }
+
+  @Override
+  public Podcast delete(String id) {
+
+    Podcast s = (Podcast) tr.delete(id);
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(DELETE_ONE_PODCAST_SQL);) {
+
+      preparedStatement.setString(1, id);
+
+      preparedStatement.executeUpdate();
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    return s;
+
+  }
+
+  @Override
+  public Podcast update(String id, Podcast podcastUpdated) {
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_ONE_PODCAST_SQL);) {
+
+      preparedStatement.setString(1, podcastUpdated.getDescription());
+
+      preparedStatement.executeUpdate();
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    return podcastUpdated;
+
+  }
+
+  @Override
+  public List<Podcast> getlist() {
+    
+    List<Podcast> songsList = new ArrayList<Podcast>();
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_PODCAST_SQL);) {
+
+      ResultSet rs = preparedStatement.executeQuery();
+
+      while (rs.next()) {
+        Podcast tmp = new Podcast(tr.get(rs.getString(1)).getName(), tr.get(rs.getString(1)).getGender(), tr.get(rs.getString(1)).getDuration());
+        tmp.setId(rs.getString(1));
+        tmp.setIdUser(tr.get(rs.getString(1)).getIdUser());
+        songsList.add(tmp);
+      }
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    return songsList;
+
+  }
+
 }
--- com.Spotify/dao/SongDAO.java
+++ com.Spotify/dao/SongDAO.java
@@ -5,36 +5,135 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import config.JDBCUtil;
 import dao.dao_interfaces.ISongDAO;
 import model.Song;
 
 public class SongDAO implements ISongDAO {
 
-  private String INSERT_SONG_SQL = "INSERT INTO cancion (nombre, duracion, popularidad, genero)" + "VALUES(?,?,?,?)";
+  private TrackDAO tr = new TrackDAO();
+
+  private String INSERT_SONG_SQL = "INSERT INTO cancion (id_pista)" + "VALUES(?)";
+
+  private String SELECT_ONE_SONG_SQL = "SELECT * FROM cancion " + "WHERE id_pista=?;";
+
+  private String DELETE_ONE_SONG_SQL = "DELETE FROM cancion " + "WHERE id_pista=?;";
+
+  private String UPDATE_ONE_SONG_SQL = "UPDATE cancion " + "SET letra WHERE id_pista = ?;";
+
+  private String SELECT_SONG_SQL = "SELECT * FROM cancion";
 
   @Override
   public Song add(Song song) {
 
-    return null;
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(INSERT_SONG_SQL,
+            java.sql.Statement.RETURN_GENERATED_KEYS);) {
+
+      preparedStatement.setString(1, tr.add(song).getId());
+      preparedStatement.setString(2, song.getLetter());
+
+      System.out.println(preparedStatement);
+
+      preparedStatement.executeUpdate();
+
+    } catch (SQLException e) {
+      System.out.println(e);
+    }
+
+    return song;
+
   }
 
   @Override
   public Song get(String id) {
-    // TODO Auto-generated method stub
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ONE_SONG_SQL);) {
+
+      preparedStatement.setString(1, id);
+  
+      ResultSet rs = preparedStatement.executeQuery();
+      while (rs.next()) {
+        return new Song(rs.getFloat(1), rs.getString(2), rs.getString(3));
+      }
+
+      rs.close();
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
     return null;
+
   }
 
   @Override
   public Song delete(String id) {
-    // TODO Auto-generated method stub
-    return null;
+
+    Song s = (Song) tr.delete(id);
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(DELETE_ONE_SONG_SQL);) {
+
+      preparedStatement.setString(1, id);
+      
+      preparedStatement.executeUpdate();
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    return s;
   }
 
   @Override
-  public Song update(String id, Song song) {
-    // TODO Auto-generated method stub
-    return null;
+  public Song update(String id, Song songUpdated) {
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_ONE_SONG_SQL);) {
+
+      preparedStatement.setString(1, songUpdated.getLetter());
+
+      preparedStatement.executeUpdate();
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    return songUpdated;
+
   }
+
+  @Override
+  public List<Song> getlist() {
+    List<Song> songsList = new ArrayList<Song>();
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_SONG_SQL);) {
+
+      ResultSet rs = preparedStatement.executeQuery();
+
+      while (rs.next()) {
+        Song tmp = new Song(tr.get(rs.getString(1)).getDuration(), tr.get(rs.getString(1)).getName(), tr.get(rs.getString(1)).getGender());
+        tmp.setId(rs.getString(1));
+        tmp.setIdUser(tr.get(rs.getString(1)).getIdUser());
+        songsList.add(tmp);
+      }
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+    return songsList;
+  }
+
 }
