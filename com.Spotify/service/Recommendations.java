7eae1b977fdd84efda20079467e71b9912ad1b26
Facu <69550808+Faq-hue@users.noreply.github.com>
2021-06-14 08:34:17 -0300

    Add files via upload

--- com.Spotify/service/Recommendations.java
+++ com.Spotify/service/Recommendations.java
@@ -1,22 +1,60 @@
 package service;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
+import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
+
 import dao.PlaylistDAO;
-import dao.trackdao.SongDAO;
+import dao.SongDAO;
 import model.Song;
+import model.Track;
 import model.Playlist;
 
-public class Recommendations {
+public class Recommendations  {
 
-  public Playlist recommendationPlaylistSong() {
+    public Playlist recommendationByCountry(){
 
-    //PlaylistDAO plDAO = new PlaylistDAO();
-    //SongDAO sDAO = new SongDAO();
+        PlaylistDAO plDAO = new PlaylistDAO();
+        SongDAO sDAO = new SongDAO();
 
-    //List<Song> listSongs = sDAO.getlist();
+        List<Song> listSongs = sDAO.getlist();        
 
-    return new Playlist();
-  }
+        return new Playlist();
+    }
 
+    public Playlist recomendationByGender(){
+
+        return new Playlist();
+    }
+
+    public Playlist recomendationByArtist(){
+
+        return new Playlist();
+    }
+
+    public Playlist recomendationByPopularity(){
+
+        Playlist pl = new Playlist();
+
+        PlaylistDAO plDAO = new PlaylistDAO();
+        
+        List<Song> listSongs = new SongDAO().getlist();
+
+        Song [] arr = new Song[listSongs.size()];
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            
+            System.out.println(arr[i]);
+
+        }
+        
+
+        return new Playlist();
+    }
+    
 }
