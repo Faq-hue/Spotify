8ec5e9366cd9f0b2ef4112d5e6a990beb0bd223c
Facu <69550808+Faq-hue@users.noreply.github.com>
2021-06-14 22:23:59 -0300

    Add files via upload

--- com.Spotify/service/Recommendations.java
+++ com.Spotify/service/Recommendations.java
@@ -3,140 +3,109 @@ package service;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
-
+import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
+import com.mysql.cj.x.protobuf.MysqlxExpr.Object;
 import dao.PlaylistDAO;
 import dao.SongDAO;
 import dao.TrackDAO;
+import dao.UserDAO;
 import model.Song;
 import model.Track;
+import model.User;
 import model.Playlist;
 
-public class Recommendations  {
+public class Recommendations {
 
-    public Playlist recommendationByCountry(){
+    public Playlist recommendationByCountry() {
+
+        return new Playlist();
+    }
+
+    public static Playlist recomendationByGender() {
+
+        List<Track> trackList = new TrackDAO().getlist();
+
+        List<Track> genderList = new ArrayList<Track>();
+
+        return new Playlist();
+    }
+
+    public static Playlist recomendationByArtist() {
+
+        Playlist pl = recomendationByPopularity();
+
+        Track t = pl.getPlaylistContent().get(0);
+
+        User u = new UserDAO().get(t.getIdUser());
+
+        List <Track> tracklist = new TrackDAO().getlist();
+
+        List<Track> definitiveList = new ArrayList<Track>();
+
+
+        for (int i = 0; i < tracklist.size(); i++) {
+
+            if( tracklist.get(0).getIdUser().equals(t.getIdUser()) ){
+
+                definitiveList.add(tracklist.get(i));
+                
+            }
+
+        }
+
+        Playlist plDef = new Playlist();
+        plDef.setNamePlaylist("Best Artist " + u.getUserName());
+        plDef.setPlaylistContent(definitiveList);
+
+        return plDef;
+    }
+
+    public static Playlist recomendationByPopularity() {
 
         List<Track> listTracks = new TrackDAO().getlist();
 
-        List<Song> listSongs = new SongDAO().getlist();
+        Track[] arr = new Track[listTracks.size()];
 
-        Track[] arr = new Track[listSongs.size()];
+        int x = 0;
+
+        Track discount = new Track();
+        discount.setPopularity(-1);
+        discount.setName("discount");
 
         for (int i = 0; i < listTracks.size(); i++) {
 
-            try {
-                if (listSongs.get(i) != null) {
+            if (listTracks.get(i).getTipe() == 0) {
 
-                    arr[i] = listTracks.get(i);
+                arr[i] = listTracks.get(i);
 
-                }
-            } catch (Exception e) {
+            } else {
 
+                arr[i] = discount;
+                x++;
             }
 
         }
 
         Arrays.sort(arr);
 
-        for (int i = arr.length; i == arr.length || i > 0; i--) {
+        List<Track> definitiveList = new ArrayList<Track>();
 
-            System.out.println(arr[i - 1]);
+        int k = 0;
 
-        }    
+        for (int i = arr.length - 1; i >= 0 + x; i--) {
 
-        return new Playlist();
-    }
-    
-
-    public Playlist recomendationByGender(){
-        List<Track> listTracks = new TrackDAO().getlist();
-
-        List<Song> listSongs = new SongDAO().getlist();
-
-        Track[] arr = new Track[listSongs.size()];
-
-        for (int i = 0; i < listTracks.size(); i++) {
-
-            try {
-                if (listSongs.get(i) != null) {
-
-                    arr[i] = listTracks.get(i);
-
-                }
-            } catch (Exception e) {
-
-            }
-
+            definitiveList.add(k, arr[i]);
+            k++;
         }
-
-        Arrays.sort(arr);
-
-        for (int i = arr.length; i == arr.length || i > 0; i--) {
-
-            System.out.println(arr[i - 1]);
-
-        }
-        
-        return new Playlist();
-    }
-
-
-    public Playlist recomendationByArtist(){
-
-      
-        List<Track> listTracks = new TrackDAO().getlist();
-
-        List<Song> listSongs = new SongDAO().getlist();
-
-        Track[] arr = new Track[listSongs.size()];
-
-        for (int i = 0; i < listTracks.size(); i++) {
-
-            try {
-                if (listSongs.get(i) != null) {
-
-                    arr[i] = listTracks.get(i);
-
-                }
-            } catch (Exception e) {
-
-            }
-
-        }
-
-        Arrays.sort(arr);
-
-        for (int i = arr.length; i == arr.length || i > 0; i--) {
-
-            System.out.println(arr[i - 1]);
-
-        }
-        
-        return new Playlist();
-    }
-
-    public Playlist recomendationByPopularity(){
 
         Playlist pl = new Playlist();
+        pl.setNamePlaylist("Best of the best");
+        pl.setPlaylistContent(definitiveList);
 
-        PlaylistDAO plDAO = new PlaylistDAO();
-        
-        List<Song> listSongs = new SongDAO().getlist();
-
-        Song [] arr = new Song[listSongs.size()];
-
-        Arrays.sort(arr);
-
-        for (int i = 0; i < arr.length; i++) {
-            
-            System.out.println(arr[i]);
-
-        }
-        
-
-        return new Playlist();
+        return pl;
     }
-    
+
 }
