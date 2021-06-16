3064692cab83c12beb0e7892c16d4142d617fb1c
Angelo Manfredi <69202121+angelo59930@users.noreply.github.com>
2021-06-16 19:48:20 -0300

    Add files via upload

--- com.Spotify/dao/PlaylistDAO.java
+++ com.Spotify/dao/PlaylistDAO.java
@@ -5,9 +5,12 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
 import config.JDBCUtil;
 import dao.dao_interfaces.track_interfaces.IPlaylist;
+import dao.TieneDAO;
 import model.Playlist;
 
 public class PlaylistDAO implements IPlaylist {
@@ -15,13 +18,16 @@ public class PlaylistDAO implements IPlaylist {
   String INSER_PLAYLIST_SQL = "INSERT INTO playlist (id_playlist,nombre,cantidad_pistas,fecha_creacion,id_usuario)"
       + " VALUES (?,?,?,?,?);";
   String SELECT_ONE_PLAYLIST = "SELECT * FROM playlist" + " WHERE id_playlist=?;";
+  String DELETE_PLAYLIST_SQL = "DELETE FROM playlist" + "WHERE id_playlist=?;";
+  String SELECT_PLAYLIST_SQL = "SELECT * FROM playlist;";
 
   @Override
   public Playlist add(Playlist playlist) {
-    try (Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
-        JDBCUtil.getPassword());
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
         PreparedStatement preparedStatement = connection.prepareStatement(INSER_PLAYLIST_SQL);) {
-      
+
       preparedStatement.setString(1, playlist.getId());
       preparedStatement.setString(2, playlist.getNamePlaylist());
       preparedStatement.setInt(3, playlist.getPlaylistContent().size());
@@ -61,7 +67,7 @@ public class PlaylistDAO implements IPlaylist {
         p.setDate(rs.getString(4));
 
         for (int i = 0; i < rs.getInt(3); i++) {
-          p.getPlaylistContent().add(new SongDAO().get(new TieneDAO().getTrack(rs.getString(1)).get(i)));
+          p.getPlaylistContent().add(new SongDAO().get(new TieneDAO().getTrackid(p.getId()).get(i)));
         }
 
       }
@@ -75,21 +81,58 @@ public class PlaylistDAO implements IPlaylist {
   }
 
   @Override
-  public Playlist delete(String arg) {
-    // TODO Auto-generated method stub
-    return null;
+  public Playlist delete(String id) {
+    Playlist p = get(id);
+    TieneDAO t = new TieneDAO();
+    t.delete(id);
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(DELETE_PLAYLIST_SQL)) {
+
+      preparedStatement.setString(1, id);
+
+      System.out.println(preparedStatement);
+
+      preparedStatement.executeUpdate();
+
+    } catch (SQLException e) {
+      System.out.println(e);
+    }
+    return p;
   }
 
   @Override
-  public Playlist update(String arg1, Playlist arg2) {
-    // TODO Auto-generated method stub
-    return null;
+  public Playlist update(String id, Playlist plUpdate) {
+    delete(id);
+    add(plUpdate);
+    return plUpdate;
   }
 
   @Override
   public List<Playlist> getlist() {
-    // TODO Auto-generated method stub
-    return null;
+
+    List<Playlist> lPlaylist = new ArrayList<>();
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_PLAYLIST_SQL);) {
+
+      ResultSet rs = preparedStatement.executeQuery();
+
+      while (rs.next()) {
+        Playlist tmp = new PlaylistDAO().get(rs.getString(1));
+        tmp.setId(rs.getString(1));
+        lPlaylist.add(tmp);
+      }
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
+    return lPlaylist;
   }
 
 }
--- com.Spotify/dao/TieneDAO.java
+++ com.Spotify/dao/TieneDAO.java
@@ -7,15 +7,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import config.JDBCUtil;
 
+import config.JDBCUtil;
+import model.Song;
 
 public class TieneDAO {
 
   public void add(String idP, String idT) {
-
     String INSERT_tiene_SQL = "INSERT INTO tiene (id_playlist,id_pista)" + " VALUES (?,?);";
-
     try (
         Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
             JDBCUtil.getPassword());
@@ -29,17 +28,17 @@ public class TieneDAO {
       System.out.println(e);
     }
   }
-  public List<String> getPlaylist(String idT) {
 
-    String SELECT_tiene_Playlist_SQL = "SELECT id_playlist FROM tiene"+" WHERE id_playlist =?;";
+  public List<String> getTrackid(String idPl) {
+    String SELECT_tiene_Track_SQL = "SELECT id_pista FROM tiene" + " WHERE id_playlist =?;";
     List<String> pl = new ArrayList<String>();
 
     try (
         Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
             JDBCUtil.getPassword());
-        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_tiene_Playlist_SQL);) {
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_tiene_Track_SQL);) {
 
-      preparedStatement.setString(1, idT);
+      preparedStatement.setString(1, idPl);
       ResultSet rs = preparedStatement.executeQuery();
       while (rs.next()) {
         pl.add(rs.getString(1));
@@ -50,27 +49,56 @@ public class TieneDAO {
     }
     return pl;
   }
-  
-  public List<String> getTrack(String idP) {
 
-    String SELECT_tiene_Track_SQL = "SELECT id_pista FROM tiene"+" WHERE id_playlist =?;";
-    List<String> tr = new ArrayList<String>();
-
+  public void delete(String idPl) {
+    String DELETE_PLAYLIST_SQL = "DELETE FROM tiene" + "WHERE id_playlist=?;";
     try (
         Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
             JDBCUtil.getPassword());
-        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_tiene_Track_SQL);) {
-        
-      preparedStatement.setString(1, idP);
-      ResultSet rs = preparedStatement.executeQuery();
-      while(rs.next()){
-        tr.add(rs.getString(1));
-      }
+        PreparedStatement preparedStatement = connection.prepareStatement(DELETE_PLAYLIST_SQL)) {
+
+      preparedStatement.setString(1, idPl);
+
+      System.out.println(preparedStatement);
+
+      preparedStatement.executeUpdate();
 
     } catch (SQLException e) {
       System.out.println(e);
     }
-    return tr;
+
   }
+
+  public List<String> getList(String id) {
+    List<String> ls = new ArrayList<String>();
+    String SELECT_ALL_SQL = "SELECT id_pista FROM tiene" + "WHERE id_playlist=?";
+
+    try (
+        Connection connection = DriverManager.getConnection(JDBCUtil.getURL(), JDBCUtil.getUser(),
+            JDBCUtil.getPassword());
+        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_SQL);) {
+      
+      preparedStatement.setString(1, id);
+      ResultSet rs = preparedStatement.executeQuery();
+
+      while (rs.next()) {
+        ls.add(rs.getString(1));
+      }
+
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
+    return ls;
+  }
+
+  public void update(String id,List<Song> lS) {
+    if(getList(id).size()>lS.size()){
+
+    }
+  
+  }
+
+
 
 }
